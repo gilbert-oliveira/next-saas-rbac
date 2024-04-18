2d670406289857f1921cbac69a6ef3610eb62f74
Gilbert <contato@gilbert.dev.br>
2024-04-18 08:57:44 -0300

    feat: Setup das permissões - Permissões por cargos

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,6 @@
-import { ability } from '@saas/auth'
+import { defineAbilitiesFor } from '@saas/auth'
+
+const ability = defineAbilitiesFor({ role: 'MEMBER' })
 
 const userCanInviteSomeoneElse = ability.can('invite', 'User')
 const userCannotDeleteSomeoneElse = ability.cannot('delete', 'User')
