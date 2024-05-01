6356bca76f36d33f2ef3a16746c9d0aa5f86c869
Gilbert <contato@gilbert.dev.br>
2024-04-30 22:44:36 -0300

    feat: Setup das permissões - Definindo permissões do app

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,9 @@
-import { defineAbilitiesFor } from '@saas/auth'
+import { defineAbilitiesFor, projectSchema } from '@saas/auth'
 
-const ability = defineAbilitiesFor({ role: 'MEMBER' })
+const ability = defineAbilitiesFor({ role: 'MEMBER', id: 'user-id' })
 
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-const userCannotDeleteSomeoneElse = ability.cannot('delete', 'User')
+const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
 
-console.log(userCanInviteSomeoneElse)
-console.log(userCannotDeleteSomeoneElse)
+console.log(ability.can('get', 'Billing'))
+console.log(ability.can('create', 'Invite'))
+console.log(ability.can('delete', project))
