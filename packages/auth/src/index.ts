24aea89d36f94b284b6b373bb5bc27b935661b70
Gilbert <contato@gilbert.dev.br>
2024-04-30 21:47:08 -0300

    fix: billingSuject

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -8,6 +8,7 @@ import { z } from 'zod'
 
 import { User } from './models/user'
 import { permissions } from './permissions'
+import { billingSubject } from './subjects/billing'
 import { inviteSubject } from './subjects/invite'
 import { organizationSubject } from './subjects/organization'
 import { projectSubject } from './subjects/project'
@@ -18,6 +19,7 @@ const appAbilitiesSchema = z.union([
   userSubject,
   organizationSubject,
   inviteSubject,
+  billingSubject,
   z.tuple([z.literal('manage'), z.literal('all')]),
 ])
 
